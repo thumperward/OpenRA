9a5c5ebb63c0fe4887032ea5e2988983a7839ac0
Chris Cunningham <thumperward@hotmail.com>
2023-10-02 20:09:04 +0000

    rename windows cross builds

--- packaging/package-all.sh
+++ packaging/package-all.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -euo pipefail
 
-targets=$([[ "$OSTYPE" == "darwin*" ]] && echo "macos" || echo "windows linux source")
+targets=$([[ "$OSTYPE" == "darwin*" ]] && echo "macos" || echo "windows_cross linux source")
 
 for target in $targets; do
   ("$(dirname "$0")/$target/buildpackage.sh" "$1" "$2")
